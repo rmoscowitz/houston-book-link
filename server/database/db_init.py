569c873dffd3f935513874613e95c579de3ab71a
Cameron Alexander <emptyflash@gmail.com>
2017-05-20 15:24:02 -0500

    Add library table and more fields to books

--- server/database/db_init.py
+++ server/database/db_init.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy.dialects.postgres import JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -10,13 +11,30 @@ CONN = create_engine("postgresql+psycopg2://{}:{}@/{}".format(USER, PASSWORD, DB
 
 Base = declarative_base()
 
+class Library(Base):
+    __tablename__ = "libraries"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    overdrive_id = Column(String)
+    name = Column(String)
+    collection_token = Column(String)
+
 
 class Book(Base):
-    __tablename__ = 'books'
+    __tablename__ = "books"
 
-    title = Column(String, primary_key=True)
-    director = Column(String)
-    year = Column(String)
+    id = Column(Integer, primary_key=True)
+    overdrive_id = Column(String)
+    media_type = Column(String)
+    title = Column(String)
+    subtitle = Column(String)
+    sort_title = Column(String)
+    primary_creator_role = Column(String)
+    primary_creator_name = Column(String)
+    star_rating = Column(String)
+    date_added = Column(DateTime)
+    response = Column(JSON)
+    library_id = Column(Integer, ForeignKey("library.id"))
 
 
 Base.metadata.create_all(CONN)
