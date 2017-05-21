00a67549f86e09164099fa850145ceba3d213e19
Cameron Alexander <emptyflash@gmail.com>
2017-05-21 18:20:58 -0500

    Move overdrive auth token to an env var

--- extractor/main.py
+++ extractor/main.py
@@ -17,7 +17,7 @@ COLLECTIONS = ["v1L1BQAAAAA2e", "v1L1BLQAAAA2p"]
 BASE = "https://api.overdrive.com/v1/collections/{collection}/products?limit=300&offset={offset}"
 HEADERS = {
     "User-Agent": "MOSCOWITZTX",
-    "Authorization": "Bearer AAEAAGch55ePvsd_HtZjkEV_CyYUskF-jYux8u8oe92_MnbuHQ_0HtqKhndJrdYpxeYvhZm0drcZ8UvT_KCaHvxW8L0wEm74i0NXpgO4WeU_GkrHMSX09QJJAers6CrOLYjZSzLkvqdtlPZ2r38UijN4rIcO9fzppC8rkwcrDLimPfjNUFuEIxsKyE0zpXyFpylBQpAF4ocY_YVqh2tPR5sumFCKJ3q5BPqLhQS_l6VO-4X5bB-SQ2_Dll7s6JFRtfqvlkObPq9SNqEdRt5KUe82mIGCkXpNtmjuu4IkxFu0UhzYA5ghAq7zTC9YqYx6vsDXR0Un9mBWS5aVvJPTHqe-hJuEAQAAAAEAADaSXqAxUfQnjpE2AyrMLNxNzPDtb9hmg_WC9bUbwav3jIbH3giQeXcqZJsgA16ABEJnQi4ZcHB56xtCdia-Edc6wCH_HQuPxjKbdHG5z1Da-SAWUcHCuosFoRlWulR7GstR-R0YvnzZt8deCKbHlBqeoyH7DAu3v616vIatwyyc74wQSKHnhQo8Z8g4VkJ2QyQ9Jric64zIu0Ssxp0zDqFZLMfEZZFUAi5NBElhGZhePMeW6nhMVtqgpef_Anq-LKuHWPMXR-UHdLH43oQSIGJ-esSRq8-mLUT-XFYOZh8_QTfSSAiANC6QPi5tteJ5aoMHchZ2peebRSV7xeE0KOo5D_WUxuoP0qeiFLJ9mUQPVbkVg_HushkMHQhPl6vElFINEJq3I__UpYVxcdPvSk9Dk5fP7zxMrSl6K4fxC8GDgzg65SztDpavYLSbetCEJJ8-GptZDaamTYj6aKIHPPJJkxYZfLkidmINVUKubnifgoikJMcC9O5pRdvLhFgyUw"
+    "Authorization": "Bearer {}".format(os.environ["OVERDRIVE_AUTH_TOKEN"])
 }
 OFFSET_SCALAR = 300
 
